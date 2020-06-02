bf4225f1582cb6b7a844a3a8e547f12223258ba1
Livia Seibert <livia.seibert@duke.edu>
2020-06-02 20:49:16 +0000

    indentation changes and switched br to css

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -44,17 +44,19 @@ function addMessageToDom(message) {
 document.addEventListener('DOMContentLoaded', getComments);
 
 function getComments() {
-  fetch('/data').then((response) => response.json()).then((comments) => {
-      const commentsListElement = document.getElementById('comments-section');
-      if (comments.length == 0) {
-        commentsListElement.innerHTML = 'Nothing to show.';
-      } else {
-        commentsListElement.innerHTML = '';
-        comments.map(createCommentElement).forEach(element => commentsListElement.appendChild(element));
-      }
-      }).catch(() => {
-        console.log("error");
-  });
+  fetch('/data').then((response) => response.json())
+                .then((comments) => {
+                  const commentsListElement = document.getElementById('comments-section');
+                  if (comments.length == 0) {
+                    commentsListElement.innerHTML = 'Nothing to show.';
+                  } else {
+                    commentsListElement.innerHTML = '';
+                    comments.map(createCommentElement).forEach(element => commentsListElement.appendChild(element));
+                  }
+                })
+                .catch(() => {
+                  console.log("error");
+                });
 }
 
 /**
