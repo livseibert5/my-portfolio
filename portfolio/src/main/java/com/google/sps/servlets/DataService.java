6fd94177b15b3ed398ea8ac17def846a66cdc1d6
Livia Seibert <livia.seibert@duke.edu>
2020-06-08 22:10:28 +0000

    fixed stream import statement

--- portfolio/src/main/java/com/google/sps/servlets/DataService.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataService.java
@@ -27,7 +27,7 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream;
+import java.util.stream.Collectors;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
