90b57f8fd623b2102c1704e545d977a3b7be4546
Livia Seibert <livia.seibert@duke.edu>
2020-06-08 16:49:27 +0000

    added a basic map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -86,3 +86,9 @@ function createCommentElement(name, text) {
 
   return commentElement;
 }
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8});
+  }
